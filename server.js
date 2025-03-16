39fee098d1ddedac0beb892a1bcd2a5a27dbe7ae
AIdysh Baldan <aidysh0324@gmail.com>
2025-03-16 23:45:15 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use("", express.static(__dirname + "/src/views/home"));
+app.get("", (req, res) => {
+    res.sendFile(__dirname + "/src/views/home/index.html");
+});
+
+// ПОРТ ПРИЛОЖЕНИЯ
+
+app.listen(3000, () => {
+    console.log("СЕРВЕР РАБОТАЕТ НА ПОРТУ 3000");
+    console.log("http://localhost:3000");
+});
